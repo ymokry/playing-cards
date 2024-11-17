f9ad149851190655be419547e8a492b1bba10d9b
Yauheni Mokry <github@sl.mokry.me>
2024-11-17 23:56:22 +0100

    feat: Extend file utils

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
 import type { BunFile } from "bun";
-import { type CardSuit, type CardRank } from "@/data/constants";
+import { type CardSuit, type CardRank, type CourtCard } from "@/data/constants";
 import paths from "@/data/paths";
 
 type GetAssetOptions =
   | { type: "suit"; suit: CardSuit }
-  | { type: "rank"; rank: CardRank };
+  | { type: "rank"; rank: CardRank }
+  | { type: "court"; court: CourtCard };
 
 export const getAsset = (options: GetAssetOptions): BunFile => {
   let assetPath: string;
@@ -17,6 +18,10 @@ export const getAsset = (options: GetAssetOptions): BunFile => {
     }
     case "rank": {
       assetPath = path.resolve(paths.assets, "ranks", `${options.rank}.svg`);
+      break;
+    }
+    case "court": {
+      assetPath = path.resolve(paths.assets, "courts", `${options.court}.svg`);
       break;
     }
   }
