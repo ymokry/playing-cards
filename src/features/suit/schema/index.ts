0e6fd13ff89e900a52363839bb5f41d5c7169acc
Yauheni Mokry <github@sl.mokry.me>
2024-11-17 14:59:34 +0100

    refactor: Optimize suit schema

--- src/features/suit/schema/index.ts
+++ src/features/suit/schema/index.ts
@@ -1,25 +1,21 @@
 import { z } from "zod";
 import { schema, constants } from "@/features/svg";
 
-const svgAttributesSchema =
-  schema.elements[constants.ElementNames.SVG].attributes;
 const pathAttributesSchema =
   schema.elements[constants.ElementNames.PATH].attributes;
 
-const suitBaseAttributesSchema = svgAttributesSchema.pick({
-  [constants.AttributeNames.VIEW_BOX]: true,
-});
 const suitPathBaseAttributesSchema = pathAttributesSchema.pick({
   [constants.AttributeNames.D]: true,
 });
 
 const suitSvgSchema = z.object({
   [constants.ElementNames.SVG]: z.object({
-    [constants.attributesGroupName]: suitBaseAttributesSchema
-      .merge(
-        svgAttributesSchema.pick({ [constants.AttributeNames.XMLNS]: true })
-      )
-      .required(),
+    [constants.attributesGroupName]: schema.elements[
+      constants.ElementNames.SVG
+    ].attributes.pick({
+      [constants.AttributeNames.XMLNS]: true,
+      [constants.AttributeNames.VIEW_BOX]: true,
+    }),
     [constants.ElementNames.PATH]: z.object({
       [constants.attributesGroupName]: suitPathBaseAttributesSchema,
     }),
@@ -29,9 +25,10 @@ export type SuitSVG = z.infer<typeof suitSvgSchema>;
 
 const suitSymbolSchema = z.object({
   [constants.ElementNames.SYMBOL]: z.object({
-    [constants.attributesGroupName]: suitBaseAttributesSchema.extend({
-      [constants.AttributeNames.ID]:
-        schema.attributes[constants.AttributeNames.ID],
+    [constants.attributesGroupName]: schema.elements[
+      constants.ElementNames.SYMBOL
+    ].attributes.omit({
+      [constants.AttributeNames.PRESERVE_ASPECT_RATIO]: true,
     }),
     [constants.ElementNames.PATH]: z.object({
       [constants.attributesGroupName]: suitPathBaseAttributesSchema.merge(
