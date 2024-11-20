7a541b57bf1b502f439ef607ab56e7dedf09e88e
Yauheni Mokry <github@sl.mokry.me>
2024-11-20 18:01:30 +0100

    feat: Add generate script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import Resources from "@/features/resources";
-import { prepareDist } from "@/utils/file";
+
+import { constants as suitConstants } from "@/features/suit";
+import { constants as rankConstants } from "@/features/rank";
+import getCard from "@/features/card";
+import { prepareDist, saveCard } from "@/utils/file";
 
 await Resources.load();
 await prepareDist();
+
+const deckPromises: Promise<number>[] = [
+  saveCard("cut", getCard({ type: "cut" })),
+  saveCard("back", getCard({ type: "back" })),
+];
+
+Object.values(suitConstants.SuitTypes).forEach((suit) => {
+  Object.values(rankConstants.RanksTypes).forEach((rank) => {
+    deckPromises.push(
+      saveCard(`${suit}_${rank}`, getCard({ type: "playing", suit, rank }))
+    );
+  });
+});
+
+await Promise.all(deckPromises);
